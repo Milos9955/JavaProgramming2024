876d13645d70834bb05cae5f769a98f1ba24dd56
User <cmilos9955@gmail.com>
2024-11-11 00:01:00 +0100

    Pokazano je kako mozete definirati vrednosti celog broja

---
+++ src/main/java/Two/ThirdProgram.java
@@ -0,0 +1,10 @@
+package Two;
+
+public class ThirdProgram {
+    public static void main(String[] args) {
+        int number = 0b11010;
+                //2 + 8 + 16
+        System.out.println();
+    }
+}
+
